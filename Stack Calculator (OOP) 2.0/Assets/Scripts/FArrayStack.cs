ee9691ae6d58b57336b9ad657ac68ff6b3baf65a
Bramdelooze <39378407+Bramdelooze@users.noreply.github.com>
2018-05-17 17:44:45 +0200

    Update FArrayStack.cs

--- Stack Calculator (OOP) 2.0/Assets/Scripts/FArrayStack.cs
+++ Stack Calculator (OOP) 2.0/Assets/Scripts/FArrayStack.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,7 +11,7 @@ public class FArrayStack : fStack {
     void Start ()
     {
         stackElements = new float[10];
-	}
+    }
 
     override public void Push(float number)
     {
